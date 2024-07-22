f6c5e5bed121c4969978d290a9cdcdbe5e8374aa
Duncan <njorogedun870@gmail.com>
2024-07-22 06:38:02 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = 0;
+
+const batteries = [
+  { assembled: true, count: 10 },
+  { assembled: true, count: 10 },
+  { assembled: true, count: 11 },
+];
+
+const reducer = () => {
+  totalBatteries = batteries.reduce((acc, battery) => {
+    if (battery.assembled) {
+      return acc + battery.count;
+    }
+    return acc;
+  }, 0);
+  return totalBatteries;
+};
+
+reducer(); // Call the reducer function to calculate totalBatteries
